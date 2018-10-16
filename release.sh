751c6dc4a62842dba2ddef60927d81c7b0f13407
axiaoxin <254606826@qq.com>
2018-10-16 15:06:30 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-rm -rf ./zybbixserver
+rm -rf ./zybbixserver ./zybbixserver.tar.gz
 mkdir ./zybbixserver
 go build -o ./zybbixserver/zybbixserver
 cp README.md zybbixserver.json supervisor.conf ./zybbixserver
